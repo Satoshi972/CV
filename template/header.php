4b5128c665e1365d515f5e76b15e601cb0f265d4
Satoshi972 <ruddy.marieluce@gmail.com>
2017-03-08 14:48:11 -0400

    Mon CV en php (ou angular, j'y reflechis)

---
+++ template/header.php
@@ -0,0 +1,4 @@
+<!-- contient mon le contenu de mon header -->
+	<h3>MARIE-LUCE Ruddy</h3>
+	<h3>23 ans</h3>
+	<h1>Développeur full stack</h1>
---
+++ template/sidebar.php
@@ -0,0 +1,5 @@
+<aside class=".sidebar-left jumbotron">
+	<h3>Le mole, Bat3, Esc14, P1</h3>
+	<h3>GS</h3>
+	<h1>Développeur full stack</h1>
+</aside>
